d9e94472146275f2fa189dbde21b53aad5e580c2
Sam Goodwin <sam@alchemy.run>
2026-04-09 11:56:35 -0700

    fix(cloudflare): export D1 resources and bindings

--- alchemy-effect/src/Cloudflare/index.ts
+++ alchemy-effect/src/Cloudflare/index.ts
@@ -1,4 +1,9 @@
 export * from "./Container.ts";
+<<<<<<< HEAD
+=======
+export { D1Connection } from "./D1/D1Connection.ts";
+export { Database as D1Database } from "./D1/Database.ts";
+>>>>>>> baf5be0 (fix(cloudflare): export D1 resources and bindings)
 export * from "./D1/index.ts";
 export * as KV from "./KV/index.ts";
 export * from "./Providers.ts";
